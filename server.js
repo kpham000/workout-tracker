8708d7ffb31e4e811dd77e83fab03f0282ff3fa8
kpham000 <90490117+kpham000@users.noreply.github.com>
2021-11-16 00:14:11 -0800

    the server

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3002;
+
+const app = express();
+
+const routes = require("./controllers")
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://lovalhost/workout');
+
+app.use(express.urlencoded({ extended:true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+// routes?
+
+app.listen(PORT, () => {
+    console.log(`App run on port ${PORT}!`);
+});
