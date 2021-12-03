ea8ba1a95237ddb6a017bf152ff183cf0739cf2e
kpham000 <90490117+kpham000@users.noreply.github.com>
2021-12-02 20:19:14 -0800

    add path

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../models/exerciseModel");
+const path= require("path");
+router.get("/stats", (req,res)=>{
+  res.sendFile(path.join(__dirname,"../public/stats.html"))
+})
+router.get("/exercise", (req,res)=>{
+  res.sendFile(path.join(__dirname,"../public/exercise.html"))
+})
+router.get("/", (req,res)=>{
+  res.sendFile(path.join(__dirname,"../public/index.html"))
+})
 
 router.get("/api/workouts", (req, res) => {
   db.aggregate(
